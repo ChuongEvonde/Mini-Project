3198d9d42dddc6e378d47961ba5b28858997d133
ChuongEvonde <chuongnguyen042003@example.com>
2023-05-24 17:36:05 +0700

    mini project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
-import React from 'react';
-
 const App = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+  return <div>hello</div>;
 };
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.scss'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.scss";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { BrowserRouter } from "react-router-dom";
+import { GlobalStyles } from "./styles/GlobalStyles.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <GlobalStyles></GlobalStyles>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
